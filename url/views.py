75ab9b8ea66f8b162e5090815925c3f654008906
Nataliia Maiatskaia <60422598+MIA1kl@users.noreply.github.com>
2023-08-14 05:24:50 +0600

    Update views.py

--- url/views.py
+++ url/views.py
@@ -7,8 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import redirect
 
-
-@api_view(['POST'])
+@api_view(['POST', 'GET'])  # Allow both POST and GET methods
 def urlShort(request):
     if request.method == 'POST':
         serializer = UrlDataSerializer(data=request.data)
@@ -23,9 +22,12 @@ def urlShort(request):
             data = UrlData.objects.all()
             serializer = UrlDataSerializer(data, many=True)
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':  # Handle GET requests
+        data = UrlData.objects.all()
+        serializer = UrlDataSerializer(data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-@api_view(['GET'])
+@api_view(['GET'])  # Allow only GET method
 def urlRedirect(request, shorturl):
     try:
         data = UrlData.objects.get(shorturl=shorturl)
