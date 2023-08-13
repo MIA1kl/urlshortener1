a45065ba282543d59b077415c7f10d38277ab99c
Nataliia Maiatskaia <60422598+MIA1kl@users.noreply.github.com>
2023-08-14 05:18:28 +0600

    Update views.py

--- url/views.py
+++ url/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from django.shortcuts import redirect
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def urlShort(request):
     if request.method == 'POST':
         serializer = UrlDataSerializer(data=request.data)
@@ -20,15 +20,9 @@ def urlShort(request):
             request.user.urlshort.add(new_url)
             return Response(status=status.HTTP_201_CREATED)
         else:
-            data = UrlData.objects.all()
-            serializer = UrlDataSerializer(data, many=True)
-            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        data = UrlData.objects.all()
+        serializer = UrlDataSerializer(data, many=True)
+        return Response(serializer.data)
 
-
-@api_view(['GET'])
-def urlRedirect(request, shorturl):
-    try:
-        data = UrlData.objects.get(shorturl=shorturl)
-        return redirect(data.url)
-    except UrlData.DoesNotExist:
-        return Response({'error': 'Short URL not found'}, status=status.HTTP_404_NOT_FOUND)
